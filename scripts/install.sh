d5bcf779367fb2ab0e5b86cdfdbf8f5528b44b5f
khyunjun <familykim01@naver.com>
2023-10-13 16:35:50 +0900

    빌드 관련 업데이트

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,4 @@
 cd /home/ubuntu/nextjs-app/
 
 # Start the Next.js application using PM2
-rm -rf node_modules
-rm -rf .next
+npm install
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cd /var/www/nextjs-app/
+cd /home/ubuntu/nextjs-app/
 
 # Start the Next.js application using PM2
-pm2 start npm --name "nextjs-app" -- start
+pm2 describe "nextjs-app" 2>/dev/null | grep "online" 
+if [ $? -eq 0 ]; then
+    pm2 restart "nextjs-app"
+else
+    pm2 start "npm run start" --name "nextjs-app"
+fi
