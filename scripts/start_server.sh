41122ef178aec7b1bd968be6c304e38efcf328db
khyunjun <familykim01@naver.com>
2023-10-13 16:41:24 +0900

    빌드 테스트

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-cd /home/ubuntu/nextjs-app/
+cd /home/ubuntu/nextjs-app
 
 # Start the Next.js application using PM2
-pm2 describe "nextjs-app" 2>/dev/null | grep "online" 
-if [ $? -eq 0 ]; then
-    pm2 restart "nextjs-app"
-else
-    pm2 start "npm run start" --name "nextjs-app"
-fi
+pm2 start "npm run start" --name "nextjs-app"
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -5,3 +5,4 @@ cd /home/ubuntu/nextjs-app/
 # Start the Next.js application using PM2
 rm -rf node_modules
 rm -rf .next
+rm -rf package-lock.json
