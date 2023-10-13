e196d9be81d1e7a46fca32f2a46ace03efc8eabc
khyunjun <familykim01@naver.com>
2023-10-13 17:06:24 +0900

    clean_up 동작 테스트

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+echo "Starting clean up script."
+
 cd /home/ubuntu/nextjs-app
-# Start the Next.js application using PM2
+echo "Changed directory to /home/ubuntu/nextjs-app."
+
 sudo rm -rf node_modules
+echo "Removed node_modules."
+
 sudo rm -rf .next
+echo "Removed .next."
+
 sudo rm -rf package-lock.json
+echo "Removed package-lock.json."
+
+mkdir /home/ubuntu/nextjs-app/test_directory
+touch /home/ubuntu/nextjs-app/test_file.txt
