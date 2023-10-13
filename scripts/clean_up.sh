7b4db482719873ea0df6b8afe04368ff0cecac70
khyunjun <familykim01@naver.com>
2023-10-13 16:52:31 +0900

    권한 수정

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /home/ubuntu/nextjs-app/
-
+cd /home/ubuntu/nextjs-app
+sudo chown -R ubuntu:ubuntu /home/ubuntu/nextjs-app
 # Start the Next.js application using PM2
 rm -rf node_modules
 rm -rf .next
