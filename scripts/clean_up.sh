8da4f1ba49577a24e30efd2f381a6cb6fc2b56ea
khyunjun <familykim01@naver.com>
2023-10-13 16:59:23 +0900

    sudo로 변경

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 cd /home/ubuntu/nextjs-app
-sudo chown -R ubuntu:ubuntu /home/ubuntu/nextjs-app
 # Start the Next.js application using PM2
-rm -rf node_modules
-rm -rf .next
-rm -rf package-lock.json
+sudo rm -rf node_modules
+sudo rm -rf .next
+sudo rm -rf package-lock.json
