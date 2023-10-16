3468821a86eb834c5e7e49ce5fc8b33ab43a3cb2
khyunjun <familykim01@naver.com>
2023-10-16 11:08:05 +0900

    빌드 테스트2

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,9 +4,5 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v18.15.0/bin/
 
 cd /home/ubuntu/nextjs-app
 
-/home/ubuntu/.nvm/versions/node/v18.15.0/bin/pm2 describe "nextjs-app" 2>/dev/null | grep "online" 
-if [ $? -eq 0 ]; then
-    /home/ubuntu/.nvm/versions/node/v18.15.0/bin/pm2 restart "nextjs-app"
-else
-    /home/ubuntu/.nvm/versions/node/v18.15.0/bin/pm2 start "npm run start" --name "nextjs-app"
-fi
+/home/ubuntu/.nvm/versions/node/v18.15.0/bin/pm2 start "npm run start" --name "nextjs-app"
+/home/ubuntu/.nvm/versions/node/v18.15.0/bin/pm2 restart "nextjs-app"
