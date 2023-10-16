8ea7f72547d8efa869da70c90ec03271beb523a5
khyunjun <familykim01@naver.com>
2023-10-16 11:14:28 +0900

    빌드 수정

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,5 +4,5 @@ export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v18.15.0/bin/
 
 cd /home/ubuntu/nextjs-app
 
-/home/ubuntu/.nvm/versions/node/v18.15.0/bin/pm2 start "npm run start" --name "nextjs-app"
-/home/ubuntu/.nvm/versions/node/v18.15.0/bin/pm2 restart "nextjs-app"
+pm2 start "npm run start" --name "nextjs-app"
+pm2 restart "nextjs-app"
